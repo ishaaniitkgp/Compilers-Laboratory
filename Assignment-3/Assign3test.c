ec9501a05f5556838c827e67f77ccab71dc19be7
ishaaniitkgp <128364279+ishaaniitkgp@users.noreply.github.com>
2023-10-22 23:28:48 +0530

    Add files via upload

---
+++ Assignment-3/Assign3test.c
@@ -0,0 +1,85 @@
+//checking for keywords
+
+enum
+void
+while
+_Complex
+
+//checking for identifiers
+
+Ishaan Sinha
+21CS30064
+Vinayak Gupta
+21CS30064
+Aditya Varshney
+21EC10089
+Upayan Chatterjee
+21EC10091
+
+//checking for constants
+
+0987654
+5242472565425743782479647937
+63475468e+4637
+37475467e-47676
+653475.6546548674598
+.5746357
+0.4543775
+45634558.
+
+//checking for string literals
+"Ishaan Sinha"
+"Vinayak Gupta"
+"21CS30064\n"
+"21CS10077\n"
+
+//checking for punctuators
+
+[ ] ( ) { } . ->
+++ -- & * + - ~ !
+/ % << >> < > <= >= == != ^ | && ||
+? : ; ...
+= *= /= %= += -= <<= >>= &= ^= |=
+
+
+//sum in c
+
+int A, B, sum = 0;
+  
+    // Ask user to enter the two numbers
+    printf("Enter two numbers A and B : \n");
+  
+    // Read two numbers from the user || A = 2, B = 3
+    scanf("%d%d", &A, &B);
+  
+    // Calculate the addition of A and B
+    // using '+' operator
+    sum = A + B;
+  
+    // Print the sum
+    printf("Sum of A and B is: %d", sum);
+  
+    return 0;
+    
+// random checking
+
+ 'abc
+ \vg
+ "is
+// test for comments
+
+// this is a comment
+
+/* Ishaan Sinha
+   21CS30064
+   Vinayak Gupta
+   21CS10077
+*/
+
+/* this is a test whether comments work if these letters come:- ////////*/
+
+// /* checking for single line comment which consists of multiline comment in it */
+
+/* This comment does not contain closing syntax so this is a error in commenting
+
+ 
